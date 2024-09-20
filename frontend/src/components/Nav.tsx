6e88c9198006f811f442a9fece2d69d8959d5f51
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2024-09-20 12:51:43 -0700

    fix contract link

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -42,7 +42,7 @@ const Header = () => {
             Stake to Meet
           </Link>
           <div className="hidden md:flex items-center space-x-4">
-            <Link href="https://goerli.basescan.org/address/0xba18f2dc2ce0b971f33236fdf76e227bf9d8ddbd" 
+            <Link href="https://testnet.snowtrace.io/address/0xAE40A252ad7E2BFc8a86c5f8724d807F5326cd43/contract/43113/code" 
                   className="text-[#ede8f5] hover:text-[#adbbda] transition-colors"
                   target="_blank" 
                   rel="noreferrer">
